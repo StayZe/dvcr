6c94cf587758f4c5f1745bab18a4f5e7733f4ee1
StayZe <mandra.antoine@gmail.com>
2024-12-11 14:51:21 +0100

    .

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <Link href='./timer'>Timer</Link>
+      <Link href="./timer">
+        <a>Timer</a>
+      </Link>
     </div>
   );
 }
